a1da7468a70b0caf249485bd37d60422b217df7a
moinsheikh007 <mohinshei014@gmail.com>
2023-06-29 15:10:05 +0600

    Navbar Complite

--- script/app.js
+++ script/app.js
@@ -1,1 +1,7 @@
-console.log("Hello World!!");
+const navbar = document.querySelector('.header .flex .navbar');
+document.querySelector('#menu-bar').onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
